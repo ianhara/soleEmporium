1d960c2e7bf9e12811c26f3d061a26074e0b9f5b
ijmckoy <Ikeman200@gmail.com>
2024-07-11 20:19:38 -0400

    add code to login

--- server/server.js
+++ server/server.js
@@ -2,24 +2,19 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-const { authMiddleware } = require('./middleware/authMiddleware');
-const { protect } = require('./middleware/authMiddleware');  
+const { authMiddleware } = require('./utils/auth');
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const init = require('./seeder')
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
-    const context = { req };
-    await protect(context); // Apply the auth middleware
-    return context;
-  }
 });
 
-// Create a new instance of an Apollo Server with the GraphQL schema
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
 
@@ -31,10 +26,8 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    // In production, serve the React app from the dist/ directory
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    // Set up a wildcard route to handle routing through React
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
@@ -49,4 +42,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-startApolloServer();
+  startApolloServer();
