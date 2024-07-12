a3e0c01ff991b682af24452e17a139a471cb21f7
ianhara <ianhara97@gmail.com>
2024-07-11 20:32:03 -0400

    deleted extraneous pages and their imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,41 +5,39 @@ import App from "./App.jsx"
 
 
 import Home from './pages/Home';
-import Explore from './pages/Explore';
-import Stores from './pages/Stores';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+
+import Stores from './pages/Cart';
+
 import Error from './pages/ErrorPage';
 
 
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <Home />
-        },{
-            path: '/explore',
-            element: <Explore />
-          }, {
-          path: '/stores',
-          element: <Stores />
-        }, {
-          path: '/login',
-          element: <Login />
-        }, {
-          path: '/signup',
-          element: <Signup />
-        },  
-      ],
-    },
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
-  );
-  
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/explore',
+        element: <Explore />
+      }, {
+        path: '/stores',
+        element: <Stores />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
